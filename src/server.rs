2674c61a1d505c324a2934ca4f62cf8839a6e74e
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-06-23 05:08:00 -0400

    Update server

--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,7 @@ use actix_web::{ App, HttpServer, web::{self, Data} };
 use std::io::{ Result };
 use tera::Tera;
 
+use actix_files as fs;
 use crate::routes;
 
 pub async fn server() -> Result<()> {
@@ -10,6 +11,7 @@ pub async fn server() -> Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(tera.clone()))
+            .service(fs::Files::new("/static", "static"))
             .configure(routes::init_routes)
     })
     .bind("127.0.0.1:8080")?
