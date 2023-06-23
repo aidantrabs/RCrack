48342e9e8c17a4b39f8af69df7e1e9cac9c9ca35
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-06-23 04:53:48 -0400

    Update server to support templates

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,18 @@
-use actix_web::{ App, HttpServer };
+use actix_web::{ App, HttpServer, web::{self, Data} };
 use std::io::{ Result };
+use tera::Tera;
 
 use crate::routes;
 
 pub async fn server() -> Result<()> {
-    HttpServer::new(|| {
+    let tera = Tera::new("templates/**/*").unwrap();
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(Data::new(tera.clone()))
             .configure(routes::init_routes)
     })
-    .bind("127.0.0.1:8080")?.run().await
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
