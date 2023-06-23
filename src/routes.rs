7f815c7312b4e3bc88c5eb5798f4e07b84c2ab94
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-06-23 04:53:28 -0400

    Update routes

--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,23 @@
-use actix_web::{ get, HttpResponse, Responder, web };
+use actix_web::{ web, get, post, HttpResponse, Responder };
+use tera::{ Tera, Context };
+
+use crate::data::FormData;
+use crate::rcrack::rcrack_sha1;
 
 #[get("/")]
-async fn test() -> impl Responder {
-    HttpResponse::Ok().body("Working")
+async fn form(tera: web::Data<Tera>) -> impl Responder {
+    let body = tera.render("form.html.tera", &Context::new()).unwrap();
+
+    HttpResponse::Ok().body(body)
+}
+
+#[post("/crack")]
+async fn crack(form_data: web::Form<FormData>) -> impl Responder {
+    let decoded = rcrack_sha1(&form_data.hash);
+    format!("Decoded result: {}", decoded)
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(test);
+    cfg.service(form);
+    cfg.service(crack);
 }
